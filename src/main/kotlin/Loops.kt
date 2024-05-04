6351ee5aaa3af8f1e08fda17102e48da7cf53e13
Palak Rajput <rajputpalak4698@gmail.com>
2024-05-04 19:21:57 +0530

    Loops

---
+++ src/main/kotlin/Loops.kt
@@ -0,0 +1,63 @@
+fun main() {
+    var a = 10
+    var b = 15
+    if (a == b) {
+        println("Both are equal")
+    } else if (a > b) {
+        println("a is greater than b")
+    } else {
+        println("b is greater than a")
+    }
+
+    // in kotlin if..else can be used as expression
+    var greeting = if (a > b) {
+        "Good morning"
+    } else {
+        "Good night"
+    }
+
+    println(greeting)
+
+    //{} can be omitted in case of only 1 if statement, act as ternary operator
+    val time = 20
+    val greeting2 = if (time < 18) "Good day." else "Good evening."
+    println(greeting2)
+
+
+    //Kotlin has when loop similar to switch in java
+    println("When")
+    var day = 1
+    var result = when (day) {
+        0 -> "Sunday"
+        1 -> "Monday"
+        2 -> "Tuesday"
+        3 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        else -> "Invalid"
+    }
+
+    println(result)
+
+    println("while")
+    var i = 1
+    while (i < 5) {
+        if(i==2) {
+            i++
+            continue
+        }
+        println(i)
+        i++
+    }
+
+    println("doWhile")
+    var j = 0
+    do {
+        println(j)
+        if(j == 4){
+            break
+        }
+        j++
+    } while (j <= 5)
+}
